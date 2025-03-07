d08a097ccbb318ba6ec821a75d2577eef4f9984f
nickscott7325 <Nick.Scott1@homeoffice.gov.uk>
2025-03-07 15:30:12 +0000

    Create testdata.R
    Test dataset

---
+++ data.R
@@ -0,0 +1,6 @@
+subject_1 <- c("B","B","B","M","M","C")
+subject_2 <- c("M","C","P","C","P","P")
+clashes <- c(5,2,4,3,2,1)
+df <- data.frame(Subject_1 = subject_1, Subject_2 = subject_2, Clashes = clashes)
+# Displaying the dataframe
+print(df)
